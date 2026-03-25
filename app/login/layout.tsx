877ad3bbb390d06b8e7792501bbb84992fa9bb03
Samir Azimov <samirazimov488@gmail.com>
2026-03-26 03:13:31 +0500

    added var

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  title: "Elite.su | Login",
+  title: "Elite.su | Login with email",
 };
 
 export default function LoginLayout({
